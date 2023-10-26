2cfa0a1cd93d0ba3c7d22807cfc0ae7714fc85fb
Aleksandr Sokolov <ad.sokolov.eptgpb.ru>
2023-10-26 17:31:16 +0300

    #105 and #119 from redmine. tasks 8.1 8.2 finished

--- public/index.php
+++ public/index.php
@@ -1,39 +1,20 @@
 <?php
+require '../src/Controllers/ProductController.php';
 require '../src/Loaders/FileCSVLoader.php';
 require '../src/Loaders/FileXMLLoader.php';
 require '../src/Loaders/FileProductLoaderCSV.php';
 require '../src/Loaders/FileProductLoaderXML.php';
 require '../src/Creators/ProductCreator.php';
-require '../src/Controllers/ProductController.php';
 require '../src/Validator/SchemeValidator/ProductSchemeValidator.php';
 require '../src/Models/Product.php';
 require '../src/Loaders/POSTProductLoader.php';
 require '../src/Loaders/POSTLoader.php';
 require '../src/Validator/ProductValidator/ProductValidator.php';
-use Creators\ProductCreator;
-use Loaders\FileCSVLoader;
-use Loaders\FileXMLLoader;
-use Loaders\FileProductLoaderCSV;
-use Loaders\FileProductLoaderXML;
-use Validator\SchemeValidator\ProductSchemeValidator;
-use Loaders\POSTProductLoader;
-use Loaders\POSTLoader;
-use Validator\ProductValidator\ProductValidator;
 
-$productSchemeValidator = new ProductSchemeValidator();
-$fileCSVLoader = new FileCSVLoader();
-$fileLoaderXML = new FileXMLLoader();
-$productCreator = new ProductCreator($productSchemeValidator);
-$FileProductLoaderXML = new FileProductLoaderXML($productSchemeValidator,$fileLoaderXML,$productCreator);
-$FileProductLoaderCSV = new FileProductLoaderCSV($productSchemeValidator,$fileCSVLoader,$productCreator);
-$POSTLoader = new POSTLoader();
-$POSTLoader = new POSTProductLoader($productSchemeValidator,$POSTLoader,$productCreator);
-//    var_dump($FileProductLoaderXML->load('dataXML.xml'));
+use Controllers\ProductController;
 
-//var_dump($FileProductLoaderXML->load('/dataXML.xml'));
-//var_dump($POSTLoader->load($_GET));
+$filepath='/dataXML.xml';
+$productController = new ProductController();
+$productsStatus = $productController->handleRequest('xml',$filepath);
 
-$productValidator = new ProductValidator();
-$validationStatus = $productValidator->validate($FileProductLoaderXML->load('/dataXML.xml'));
-
-var_dump($validationStatus);
+echo json_encode($productsStatus);
--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -1,41 +1,43 @@
 <?php
-//namespace Controllers;
-//use Creators\ProductCreator;
-//use Loaders\FileCSVLoader;
-//use Loaders\FileXMLLoader;
-//use Loaders\FileProductLoaderCSV;
-//use Loaders\FileProductLoaderXML;
-//use Validator\SchemeValidator\ProductSchemeValidator;
-//use Loaders\POSTProductLoader;
-//use Loaders\POSTLoader;
-//use Validator\ProductValidator\ProductValidator;
-//
-//class ProductController
-//{
-//
-//    public function handleRequest($dataType){
-//        $loader = null;
-//        $productValidator = new ProductValidator();
-//        $schemeValidator = new ProductSchemeValidator();
-//        $productCreator = new ProductCreator($schemeValidator);
-//
-//        switch ($dataType){
-//            case 'csv':
-//                $fileCSVLoader = new FileCSVLoader();
-//                $loader = new FileProductLoaderCSV($schemeValidator,$fileCSVLoader,$productCreator);
-//                break;
-//            case 'xml':
-//                $fileXMLLoader = new FileXMLLoader();
-//                $loader = new FileProductLoaderXML($schemeValidator,$fileXMLLoader,$productCreator);
-//                break;
-//            case 'post';
-//                $postLoader = new POSTLoader();
-//                $loader = new POSTProductLoader($schemeValidator,$postLoader,$productCreator);
-//
-//        }
-//
-//
-//    }
-//
-//
-//}
+namespace Controllers;
+use Creators\ProductCreator;
+use Loaders\FileCSVLoader;
+use Loaders\FileXMLLoader;
+use Loaders\FileProductLoaderCSV;
+use Loaders\FileProductLoaderXML;
+use Validator\SchemeValidator\ProductSchemeValidator;
+use Loaders\POSTProductLoader;
+use Loaders\POSTLoader;
+use Validator\ProductValidator\ProductValidator;
+
+class ProductController
+{
+
+    public function handleRequest($dataType,$filePath){
+        $productLoader = null;
+        $productValidator = new ProductValidator();
+        $schemeValidator = new ProductSchemeValidator();
+        $productCreator = new ProductCreator($schemeValidator);
+
+        switch ($dataType){
+            case 'csv':
+                $fileCSVLoader = new FileCSVLoader();
+                $productLoader = new FileProductLoaderCSV($schemeValidator,$fileCSVLoader,$productCreator);
+                break;
+            case 'xml':
+                $fileXMLLoader = new FileXMLLoader();
+                $productLoader = new FileProductLoaderXML($schemeValidator,$fileXMLLoader,$productCreator);
+                break;
+            case 'post';
+                $postLoader = new POSTLoader();
+                $productLoader = new POSTProductLoader($schemeValidator,$postLoader,$productCreator);
+
+        }
+
+        $products = $productLoader->load($filePath);
+
+        return $productValidator->validate($products);
+    }
+
+
+}
